7fd2889e14f7e6bc39df58a419cf9e4773a182c6
TZhang-ui <tz141@duke.edu>
2026-03-03 17:04:28 +0800

    Update how_to_run_AF3.sh

--- how_to_run_AF3.sh
+++ how_to_run_AF3.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
 
 ###############################################################################
 # AlphaFold3 Manual Workflow on DKUCC
 #
 # This script documents the standard procedure for running AlphaFold3 (AF3)
-# on the DKUCC cluster.
+# on DKUCC.
 #
 # Execute commands step-by-step as needed.
 ###############################################################################
@@ -16,6 +15,7 @@
 
 # Run this command from your local terminal:
 ssh your_netid@dkucc-login-01.rc.duke.edu
+#then you need to enter password
 
 # Example:
 # ssh tz141@dkucc-login-01.rc.duke.edu
@@ -113,3 +113,50 @@ cd ../AF3_Output
 ###############################################################################
 # End of workflow
 ###############################################################################
+
+
+
+###############################################################################
+# Example Interactive Session (Real Terminal Demonstration)
+#
+# The following example shows a real execution of the workflow on DKUCC.
+# Commands are prefixed by the shell prompt for clarity.
+###############################################################################
+
+# Local machine login
+zhangtianyue@MacBook-Pro-39 ~ % ssh tz141@dkucc-login-01.rc.duke.edu
+(tz141@dkucc-login-01.rc.duke.edu) Password:
+Last login: Mon Mar  2 21:42:52 2026 from 10.203.145.200
+
+tz141@dkucc-login-01  ~ $ cd /work/tz141
+tz141@dkucc-login-01  /work/tz141 $ ls
+AF3_Input  AF3_Output  run_AF3_tz141.sh
+
+tz141@dkucc-login-01  /work/tz141 $ cd AF3_Input
+
+tz141@dkucc-login-01  /work/tz141/AF3_Input $ sftp test@10.200.20.112
+Connected to 10.200.20.112.
+
+sftp> cd /u02
+sftp> cd tianyueZhang/
+sftp> cd 7788
+sftp> cd af3_test/
+sftp> get *json
+
+Fetching /u02/tianyueZhang/7788/af3_test/pro1.json to pro1.json
+Fetching /u02/tianyueZhang/7788/af3_test/pro2.json to pro2.json
+Fetching /u02/tianyueZhang/7788/af3_test/pro3.json to pro3.json
+
+sftp> exit
+
+tz141@dkucc-login-01  /work/tz141/AF3_Input $ sbatch /work/tz141/run_AF3_tz141.sh
+Submitted batch job 261
+
+tz141@dkucc-login-01  /work/tz141/AF3_Input $ squeue
+
+             JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)
+               261   h20-gpu   TYTYTY    tz141  R       0:03      1 dkucc-core-gpu-dkurc-z-it13-05
+
+tz141@dkucc-login-01  /work/tz141/AF3_Input $ cd ..
+tz141@dkucc-login-01  /work/tz141 $ cd AF3_Output
+tz141@dkucc-login-01  /work/tz141/AF3_Output $ ls
